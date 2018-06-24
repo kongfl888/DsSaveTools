f882aa46da3cadaa79ed18b3c1d33fd8539ed662
kongfl888 <kongfl888@outlook.com>
2018-06-25 07:51:04 +0800

    Update to 1.1.8.2

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,10 +2,10 @@ using System.Runtime.InteropServices;
 using System;
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.8.1")]
+[assembly: AssemblyVersion("1.1.8.2")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCopyright("(C)2016 @kongfl888.  (C)2007-2009 @Anter. All rights reserved.")]
+[assembly: AssemblyCopyright("(C)2016-2018 @kongfl888.  (C)2007-2009 @Anter. All rights reserved.")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyKeyFile("")]
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: AssemblyProduct("DS存档转换软件")]
 [assembly: AssemblyTitle("DS存档转换")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyFileVersion("1.1.8.1")]
+[assembly: AssemblyFileVersion("1.1.8.2")]
 [assembly: Guid("81D75630-5A9F-4121-9671-662AD8AA42F4")]
 [assembly: ComVisible(false)]
 
